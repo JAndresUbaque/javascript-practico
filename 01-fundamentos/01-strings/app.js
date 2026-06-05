6084cc16d21628602e17a9cec7768d7b197cb7f1
JAndresUbaque <jorge.ubaque55@gmail.com>
2026-06-04 20:37:18 -0500

    feat: complete string methods exercises

---
+++ 01-fundamentos/01-strings/app.js
@@ -0,0 +1,11 @@
+const userName = " Andres Ubaque ";
+const name = userName.slice().toUpperCase();
+console.log(userName);
+console.log(name);
+
+const email = "ANDRES@MAIL.COM"
+console.log(email.toLowerCase());
+
+const product = "Laptop Gamer Asus";
+console.log(product.toLowerCase().replace(/ /g,"-"));
+
